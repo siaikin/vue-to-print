96cbf3b60370cf7f963e0407715e574acf257fa1
siaikin <abc1310054026@163.com>
2023-07-20 09:52:49 +0800

    chore: 补充 package.json 中的额外信息. 增加 semantic-release 插以支持版本号修改以及 changelog 自动生成.

--- release.config.js
+++ release.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    "@semantic-release/github"
+    "@semantic-release/github",
+    "@semantic-release/git",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md"
+      }
+    ]
   ],
   repositoryUrl: "https://github.com/siaikin/vue-to-print.git"
 };
